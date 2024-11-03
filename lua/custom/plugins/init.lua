95523f64d6772b0ff38092b316a3a36d07f0baf8
Tianzong Cheng <oierprime@outlook.com>
2024-11-04 01:25:08 +0800

    feat: add dashboard

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -72,4 +72,12 @@ return {
       }
     end,
   },
+  {
+    'nvimdev/dashboard-nvim',
+    event = 'VimEnter',
+    config = function()
+      require('dashboard').setup {}
+    end,
+    dependencies = { { 'nvim-tree/nvim-web-devicons' } },
+  },
 }
